ac7bfe3c00858b0795ca48549dc7f9525609a14d
Connor McGlynn <connorappleid@intracitygeeks.org>
2018-05-29 13:54:43 -0400

    allllmost done with javascript

--- script.js
+++ script.js
@@ -31,6 +31,35 @@ $(document).ready(function() {
         });
     };
     function render(res){
-        $('#res').append("<tr id='resultdiv'><td>"+res.display_name+"</td><td>"+res.status+"</td><a target='_blank' href='"+res.url+"'><td>"+'Visit site'+"</td></a></div>");
-    }
+        $('#res').append("<tr id='resultdiv'><td><a target='_blank' href='"+res.url+"'><img src='"+res.logo+"' width='50px'></a></td><td><a target='_blank' href='"+res.url+"'>"+res.display_name+"</a></td><td class='status'><a target='_blank' href='"+res.url+"'>"+res.status+"</a></td></tr>");
+        var resObj = res;
+        for (var i in resObj) {
+            if ($('.status')[i].innerText == "null") {
+                // 
+                console.log(res.status);
+            } else {
+                // console.log(res.status);
+            }
+        }
+    };
+    function twitchStreamCheck() {
+        var userArr = ["ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"];
+        userArr.forEach(function (userArr) {
+            $.ajax({
+                url: "https://wind-bow.glitch.me/twitch-api/channels/" + userArr,
+                type: 'GET',
+                dataType: 'json',
+                success: function(res) {
+                    render(res);
+                },
+                error: function(err) {
+                    console.log(err);
+                    alert("Could not retrieve streamer data, try refreshing the page.");
+
+                }
+            });
+        });
+
+    };
+    twitchStreamCheck();
 });
