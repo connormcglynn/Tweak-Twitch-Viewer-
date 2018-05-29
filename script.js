17a092f9c1ceddec75c58b4c45b95edbf7d378ef
Connor McGlynn <connorappleid@intracitygeeks.org>
2018-05-29 11:42:51 -0400

    got channel search & JSON data display working

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
   })
+
+$(document).ready(function() {
+    $('#query').keypress(function(e){
+        if(e.which == 13){//Enter key pressed
+            $('#search-btn').click();//Trigger search button click event
+        }
+    });
+    $('#search-btn').click(function(){
+        console.log(callTwitch)
+        $('#res').html(" ");
+        callTwitch();
+    });
+    function callTwitch() {
+        var query = $('#query').val();
+        var url = "https://wind-bow.glitch.me/twitch-api/channels/" + query;
+        $.ajax({
+            url: url,
+            type: 'GET',
+            dataType: "json",
+            success: function(res) {
+                console.log(res);
+                render(res);
+            },
+            error: function(err){
+                console.log(err);
+                alert("Could not retrieve search results, try refreshing the page.");
+            }
+        });
+    };
+    function render(res){
+        $('#res').append("<tr id='resultdiv'><td>"+res.display_name+"</td><td>"+res.status+"</td><a target='_blank' href='"+res.url+"'><td>"+'Visit site'+"</td></a></div>");
+    }
+});
