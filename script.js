8c55c7dce2872c3c7455a21f83230492647ba1da
Connor McGlynn <connorappleid@intracitygeeks.org>
2018-05-31 02:20:55 -0400

    finally, finally fin-- well like 99% finished

--- script.js
+++ script.js
@@ -2,131 +2,161 @@ $(function () {
     $('[data-toggle="tooltip"]').tooltip()
   })
 
-window.onload = function() {
-    var status, url, picture, x = 0;
-    var streamArr = ["EASportsFIFA", "ESL_SC2", "OgamingSC2", "cretetion", "freecodecamp", "storbeck", "habathcx", "RobotCaleb", "noobs2ninjas"]
+const channels = ["EASportsFIFA", "ESL_SC2", "OgamingSC2", "cretetion", "storbeck", "food", "c4mlann", "ninja", "bethesda", "freecodecamp", "habathcx", "RobotCaleb", "noobs2ninjas", "overwatchleague", "shroud"]
 
-    function updateHTML(section) {
-        $('#res').append("<tr id='resultdiv'><td><a target='_blank' href='"+res.stream.channel.url+"'><img id='user-image-" + x + "' src='"+res.stream.channel.logo+"' width='50px'></a></td><td><a target='_blank' href='"+res.stream.channel.url+"'>"+res.stream.channel.display_name+"</a></td><td class='status-message'><a target='_blank' href='"+res.stream.channel.url+"'>"+status+"</a></td></tr>");
-        if (section == ".online" || section == ".offline") {
-            $("#user-image-" + x).css({background: picture, 'background-size': '55px'});
+var getData = (data) => { 
+var user = data._links.self.substr(37);
+console.log(data);
+if (data.stream != null) {
+    var logo = data.stream.channel.logo;
+    var name = data.stream.channel.display_name;
+    var game = data.stream.game;
+    var status = data.stream.channel.status;
+    $('#res').append("<tr class='online'><td><a target='_blank' href='https://www.twitch.tv/" + user + "'><img src='" + logo + "' width='50px'></a></td><td><a target='_blank' href='https://www.twitch.tv/" + user + "'>" + name + "</a></td><td><a target='_blank' href='https://www.twitch.tv/" + user + "'>" + status + "</a></td></tr>");
+} else {
+    $('#res').append("<tr class='offline'><td><a target='_blank' href='https://www.twitch.tv/" + user + "'><img src='user_placeholder.png' width='50px'></a></td><td><a target='_blank' href='https://www.twitch.tv/" + user + "'>" + user + "</a></td><td><a target='_blank' href='https://www.twitch.tv/" + user + "'>(currently offline)</a></td></tr>");
+    }
+}
+
+var ajax = (streamer) => {
+    $.ajax({
+        url: 'https://wind-bow.gomix.me/twitch-api/streams/' + streamer + "?callback=?",
+        data: {format: 'json'},
+        dataType: 'jsonp',
+        success: (json) => {
+            getData(json);
+        },
+        error: (err) => {
+            console.log(err);
         }
-        x++;
     }
+)};
 
-    function twitchChannelCheck() {
-        $.ajax({
-            url: "https://wind-bow.glitch.me/twitch-api/channels/" + streamArr[i],
-            dataType: 'jsonp',
-            data: {format: 'json'},
-            success: function(json) {
-                status = "Channel " + "'<a href='" + json.url + "' target='_blank'" + "'>" + json.display_name + "</a>'" + " is currently offline";
-                if (json.logo !== null) {
-                    picture = 'url("' + json.logo + '")';
-                } else {
-                    picture = 'url("user_placeholder.png")';
-                }
-                updateHTML(".offline");
-            }
-        });
-    };
+$(".online-btn").on('click', () => {
+    if ($(".online-btn").hasClass('active') == false) {
+        $(".all-btn").removeClass('active');
+        $(".offline-btn").removeClass('active');
+        $(".online-btn").addClass('active');
+        $(".offline").hide();
+        $(".online").show();
+    }
+});
+$(".offline-btn").on('click', () => {
+    if($(".offline-btn").hasClass('active') == false) {
+        $(".all-btn").removeClass('active');
+        $(".online-btn").removeClass('active');
+        $(".offline-btn").addClass('active');
+        $(".online").hide();
+        $(".offline").show();
+    }
+});
+$(".all-btn").on('click', () => {
+    if($(".all-btn").hasClass('active') == false) {
+        $(".online-btn").removeClass('active');
+        $(".offline-btn").removeClass('active');
+        $(".all-btn").addClass('active');
+        $(".offline").show();
+        $(".online").show();
+    }
+});
 
-    function getData(res){
-        if (res.stream === null) {
-            url = res._links.channel.substr(38);
-            twitchChannelCheck();
-        } else if (res.status == 422 || res.status == 404){
-            status = res.message;
-            updateHTML(".unavailable");
-        } else {
-            if (res.stream.channel.logo !== null) {
-                picture = 'url("' + res.stream.channel.logo + '")';
-            } else {
-                picture = 'url("user_placeholder.png")';
-            }
-            url = res._links.channel.substr(38);
-            status = "<a href='https://twitch.tv/" + url + "' target='_blank'" + "'>" + res.stream.channel.display_name +  "</a>" + " is currently streaming " + res.stream.game;
-            updateHTML(".online");
+
+$(document).ready( () => {
+    for (var i = 0; i < channels.length; i++) {
+        ajax(channels[i]);
+    }
+});
+
+
+$('#query').keypress (function(e) {
+    if (e.which == 13) {//Enter key pressed
+        $('#search-btn').click();//Trigger search button click event
+        this.select(); // selects input for deletion
+    }
+});
+$('#query').keyup (function(e) {
+    if (e.which == 8) { // backspace (delete) key reloads the page
+        console.log('works');
+        window.location.reload();
+    }
+})
+$('#search-btn').click (function() {
+    console.log(searchTwitchChannels)
+    $('#res').html(" ");
+    searchTwitchChannels();
+});
+function searchTwitchChannels() {
+    var query = $('#query').val();
+    var url = "https://wind-bow.glitch.me/twitch-api/streams/" + query;
+    $.ajax({
+        url: url,
+        type: 'GET',
+        dataType: "json",
+        success: function(res) {
+            console.log(res);
+            getData(res);
+        },
+        error: function(err){
+            console.log(err);
+            alert("Could not retrieve search results, try refreshing the page.");
         }
-    };
-
-    // Using search bar
-    $('#query').keypress(function(e){
-        if(e.which == 13){//Enter key pressed
-            $('#search-btn').click();//Trigger search button click event
-        }
-    });
-    $('#search-btn').click(function(){
-        console.log(searchTwitchStreams)
-        $('#res').html(" ");
-        searchTwitchStreams();
-    });
-    function searchTwitchStreams() {
-        var query = $('#query').val();
-        var url = "https://wind-bow.glitch.me/twitch-api/channels/" + query;
-        $.ajax({
-            url: url,
-            type: 'GET',
-            dataType: "json",
-            success: function(res) {
-                console.log(res);
-                getData(res);
-            },
-            error: function(err){
-                console.log(err);
-                alert("Could not retrieve search results, try refreshing the page.");
-            }
-        });
-    };
-
-    // AJAX call
-    function twitchStreamCheck() {
-        for (var i = 0; i < streamArr.length; i++) {
-            $.ajax({
-                url: "https://wind-bow.glitch.me/twitch-api/streams/" + streamArr[i] + "?callback=?",
-                dataType: 'jsonp',
-                data: {format: 'json'},
-                success: function(res) {
-                    console.log("Success");
-                    getData(res);
-                },
-                error: function(err) {
-                    console.log(err);
-                    alert("Could not retrieve streamer data, try refreshing the page.");
-                }
-            });
-        };
-    };
-    // twitchStreamCheck();
-    twitchChannelCheck();
-    getData();
-    function showOnline () {
-        $(".offline-users, .all-users").removeClass('focus');
-        $(".online-users").addClass('focus');
-        $(".offline, .unavailable").addClass('hidden');
-        $(".online").removeClass('hidden');
-    }
-
-    function showOffline () {
-        $(".online-users, .all-users").removeClass('focus');
-        $(".offline-users").addClass('focus');
-        $(".offline, .unavailable").removeClass('hidden');
-        $(".online").addClass('hidden');
-    }
-
-    function showAll () {
-        $(".offline-users, .online-users").removeClass('focus');
-        $(".all-users").addClass('focus');
-        $(".online, .offline, .unavailable").removeClass('hidden');
-    }
-    // Filter buttons
-    $(".online-users").click(function() {
-        showOnline();
-    });
-    $(".offline-users").click(function() {
-        showOffline();
-    });
-    $(".all-users").click(function() {
-        showAll();
     });
 };
+
+    // // // AJAX call
+    // // function twitchStreamCheck() {
+    // //     for (var i = 0; i < streamArr.length; i++) {
+    // //         $.ajax({
+    // //             url: "https://wind-bow.glitch.me/twitch-api/streams/" + streamArr[i] + "?callback=?",
+    // //             dataType: 'jsonp',
+    // //             data: {format: 'json'},
+    // //             success: function(res) {
+    // //                 console.log("Success");
+    // //                 getData(res);
+    // //             },
+    // //             error: function(err) {
+    // //                 console.log(err);
+    // //                 alert("Could not retrieve streamer data, try refreshing the page.");
+    // //             }
+    // //         });
+    // //     };
+    // // };
+    
+    // getData();
+    // function showOnline () {
+    //     $(".offline-users, .all-users").removeClass('focus');
+    //     $(".online-users").addClass('focus');
+    //     $(".offline, .unavailable").addClass('hidden');
+    //     $(".online").removeClass('hidden');
+    // }
+
+    // function showOffline () {
+    //     $(".online-users, .all-users").removeClass('focus');
+    //     $(".offline-users").addClass('focus');
+    //     $(".offline, .unavailable").removeClass('hidden');
+    //     $(".online").addClass('hidden');
+    // }
+
+    // function showAll () {
+    //     $(".offline-users, .online-users").removeClass('focus');
+    //     $(".all-users").addClass('focus');
+    //     $(".online, .offline, .unavailable").removeClass('hidden');
+    // }
+    // // Filter buttons
+    // $(".online-users").click(function() {
+    //     showOnline();
+    // });
+    // $(".offline-users").click(function() {
+    //     showOffline();
+    // });
+    // $(".all-users").click(function() {
+    //     showAll();
+    // });
+    // $(document).ready( () => {
+    //     for (var i = 0; i < streamArr.length; i++) {
+    //           ajax(streamArr[i]);
+    //     }
+    //   });
+
+
+
